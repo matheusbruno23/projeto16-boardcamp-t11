f822c8566591819230a9a18638971dbb2fa6e8dc
matheusbruno23 <matheus.bruno.1805@gmail.com>
2023-07-31 16:44:36 -0300

    correção dotenv

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import  express  from "express";
 import cors from "cors"
-import dotenv from "dotenv"
 import router from "./routes/index.routes.js"
-
-dotenv.config()
 
 const app = express()
 app.use(cors())
